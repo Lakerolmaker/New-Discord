8995bbc9be6cb726bf79523c14481907c2bdf4c7
Jacob Olsson <jacob.n.olsson@hotmail.com>
2020-07-04 23:35:59 +0200

    ui update
    added some call buttons.
    changed the devider color.
    fixed a html issue.

--- client/app/js/window.js
+++ client/app/js/window.js
@@ -272,7 +272,7 @@ function toast(heading, text) {
     text: text, // Text that is to be shown in the toast
     heading: heading, // Optional heading to be shown on the toast
     showHideTransition: 'fade', // fade, slide or plain
-    allowToastClose: true, // Boolean value true or false
+    allowToastClose: false, // Boolean value true or false
     hideAfter: 5000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
     stack: 3, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
     position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
@@ -609,6 +609,9 @@ $(document).ready(function() {
 
   $("#call-ui").hide()
   $("#main").hide()
+  $("#video_div").hide()
+
+
 
   addSocketEvents()
   addPeerjsEvents()
